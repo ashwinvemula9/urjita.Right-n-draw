5669591a0f59d711262679619d26e7b9356ec2c4
Ashwin Vemula <ashwinvem@gmail.com>
2025-03-06 09:51:04 +0530

    added comments in designer interface

--- src/pages/pdf-creators/PDFDocumentDesignerInterface.jsx
+++ src/pages/pdf-creators/PDFDocumentDesignerInterface.jsx
@@ -158,12 +158,11 @@ const styles = StyleSheet.create({
   },
 });
 
-
 const metricValues = {
-  "Dielectric Thickness" : '(Inches)',
-  "Copper Thickness" : '(OZ)',
-  "B14 Size" : '(Inches)'
-}
+  "Dielectric Thickness": "(Inches)",
+  "Copper Thickness": "(OZ)",
+  "B14 Size": "(Inches)",
+};
 
 const RuleComponent = ({ rule, isLast }) => (
   <View style={isLast ? styles.ruleBoxLast : styles.ruleBox}>
@@ -248,7 +247,10 @@ const PDFDocumentDesignerInterface = ({
               {specifications.map((spec) => (
                 <View key={spec.category_id} style={styles.infoItem}>
                   <View style={styles.infoRow}>
-                    <Text style={styles.label}>{spec.category_name} {metricValues?.[spec.category_name] || ''}:</Text>
+                    <Text style={styles.label}>
+                      {spec.category_name}{" "}
+                      {metricValues?.[spec.category_name] || ""}:
+                    </Text>
                     <Text style={styles.value}>
                       {findSpecificationName(
                         spec.category_id,
@@ -292,6 +294,7 @@ const PDFDocumentDesignerInterface = ({
               ))}
             </View>
           </View>
+          <Text style={styles.comments}>Comments: {formData.comments}</Text>
         </View>
 
         <Text
